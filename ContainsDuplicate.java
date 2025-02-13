ad04638d8024c41c912cc8ee90f269d4eedbdb35
rhythmgoyal8 <rhythmgoyal5@gmail.com>
2025-02-13 14:28:50 +0530

    solved leetcode question 217. Contains Duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        scanner.close();
+        
+        boolean result = containsDuplicate(nums);
+        System.out.println("Contains duplicate: " + result);
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
