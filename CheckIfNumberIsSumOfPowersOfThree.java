2211a1cc9de73e9bd3c97f1ccf36face81df5ba8
rhythmgoyal8 <rhythmgoyal5@gmail.com>
2025-03-04 23:25:17 +0530

    solved leetcode question 1780. Check if Number is a Sum of Powers of 3

---
+++ CheckIfNumberIsSumOfPowersOfThree.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckIfNumberIsSumOfPowersOfThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        if (checkPowersOfThree(n)) {
+            System.out.println(n + " can be represented as a sum of distinct powers of three.");
+        } else {
+            System.out.println(n + " cannot be represented as a sum of distinct powers of three.");
+        }
+    }
+
+    public static boolean checkPowersOfThree(int n) {
+        while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
