c187fd2840626b64a3749f17ab28017ecd50734d
Rhythm Goyal <rhythmgoyal5@gmail.com>
2025-06-24 09:25:30 +0530

    solved leetcode question 29. Divide Two Integers

---
+++ DivideTwoIntegers.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        // Handle overflow case
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long a = (dividend / divisor); // Division using / operator
+        return (int) a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter dividend: ");
+        int dividend = sc.nextInt();
+
+        System.out.print("Enter divisor: ");
+        int divisor = sc.nextInt();
+
+        DivideTwoIntegers solver = new DivideTwoIntegers();
+        int result = solver.divide(dividend, divisor);
+
+        System.out.println("Result of division: " + result);
+
+        sc.close();
+    }
+}
