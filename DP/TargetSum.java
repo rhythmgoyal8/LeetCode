0d3fb1edfd700a9df32f43c2779c793642a8501a
Rhythm Goyal <rhythmgoyal5@gmail.com>
2025-07-31 17:19:47 +0530

    solved leetcode question 494 Target Sum

---
+++ DP/TargetSum.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TargetSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        // Input elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Input target
+        System.out.print("Enter the target sum: ");
+        int target = sc.nextInt();
+
+        // Compute and print result
+        int result = findTargetSumWays(nums, target);
+        System.out.println("Number of ways to reach target sum: " + result);
+    }
+
+    public static int findTargetSumWays(int[] nums, int target) {
+        return rec(nums, 0, 0, target); 
+    }
+
+    private static int rec(int[] nums, int i, int val, int target) {
+        if (i >= nums.length && val == target) return 1;
+        if (i >= nums.length) return 0;
+
+        int plus = rec(nums, i + 1, val + nums[i], target);
+        int minus = rec(nums, i + 1, val - nums[i], target);
+
+        return plus + minus;
+    }
+}
